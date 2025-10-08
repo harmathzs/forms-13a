38cdefb7b11154decf665955fec5eac0fb05ecc5
Zsolt Harmath <harmathzs@gmail.com>
2025-10-08 07:51:12 +0200

    Capture submit event

--- react-app/src/pages/FileUploadPage.jsx
+++ react-app/src/pages/FileUploadPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 export default class FileUploadPage extends React.Component {
+    handleSubmit = e => {
+        e.preventDefault()
+
+        //console.log('handleSubmit e', e)
+    }
+
     render() {
         return (
             <div id="content-file" className="tab-content active">
                 <h2>File Upload</h2>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="fileInput">Choose file to upload:</label>
                     <input type="file" id="fileInput" name="fileInput" multiple />
                     <button type="submit">Upload</button>

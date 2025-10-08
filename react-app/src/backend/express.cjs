be37cebf236f64a306096765af4be3c87f2ff1a5
Zsolt Harmath <harmathzs@gmail.com>
2025-10-08 08:34:34 +0200

    Store file temporarily

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,13 +1,28 @@
 const express = require('express')
 const cors = require('cors')
+const multer = require('multer')
 
+const upload = multer({dest: 'data/'})
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/file-upload', (req, res)=>{
-    console.log('req.body', req.body)
+app.post('/file-upload', upload.single("file"), (req, res)=>{
+    console.log('req.body', req.body) // {}
+    console.log('req.file', req.file)
+    /*
+        {
+            fieldname: 'file',
+            originalname: 'almafa.txt',
+            encoding: '7bit',
+            mimetype: 'text/plain',
+            destination: 'data/',
+            filename: '37a778d57d215a5b8778c3bb0a0ce8a1',
+            path: 'data\\37a778d57d215a5b8778c3bb0a0ce8a1',
+            size: 26
+        }    
+    */
 
     res.sendStatus(200)
 })

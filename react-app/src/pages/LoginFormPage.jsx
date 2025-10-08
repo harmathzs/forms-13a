c59b4922d8095516ac1bd73e49e0de643a351b01
Zsolt Harmath <harmathzs@gmail.com>
2025-10-08 13:00:50 +0200

    Hide credentials

--- react-app/src/pages/LoginFormPage.jsx
+++ react-app/src/pages/LoginFormPage.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 
 export default class LoginFormPage extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  handleEmailChange = e => this.setState({email: e.target.value})
+  handlePasswordChange = e => this.setState({password: e.target.value})
+
+  handleSubmit = e => {
+    e.preventDefault()
+    // validate client-side
+  }
+
   render() {
     return (
       <div id="content-login" className="tab-content active">
         <h2>Login</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="loginEmail">Email:</label>
-          <input type="email" id="loginEmail" name="loginEmail" autoComplete="username" />
+          <input type="email" id="loginEmail" name="loginEmail" autoComplete="username" 
+            onChange={this.handleEmailChange}
+          required />
           <label htmlFor="loginPassword">Password:</label>
-          <input type="password" id="loginPassword" name="loginPassword" autoComplete="current-password" />
+          <input type="password" id="loginPassword" name="loginPassword" autoComplete="current-password" 
+            onChange={this.handlePasswordChange}
+          required />
           <button type="submit">Login</button>
         </form>
         <hr style={{ margin: "24px 0" }} />
@@ -20,5 +37,9 @@ export default class LoginFormPage extends React.Component {
         </div>
       </div>
     );
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('update email to: ', this.state.email)
   }
 }

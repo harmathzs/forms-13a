ee024be9165908f6d418d836efa344ebae586bd4
Zsolt Harmath <harmathzs@gmail.com>
2025-10-08 08:00:26 +0200

    Warn user if no file was selected

--- react-app/src/pages/FileUploadPage.jsx
+++ react-app/src/pages/FileUploadPage.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 export default class FileUploadPage extends React.Component {
+    state = {
+        selectedFile: null
+    }
+
+    handleFileSelect = e => {
+        //console.log('handleFileSelect files', e.target.files[0])
+
+        const selectedFile = e.target.files[0]
+        this.setState({selectedFile})
+    }
+
     handleSubmit = e => {
         e.preventDefault()
+        if (!this.state.selectedFile) {
+            console.warn('Hey, no file is selected!')
+            return null
+        }
 
-        //console.log('handleSubmit e', e)
+        
     }
 
     render() {
@@ -12,7 +27,9 @@ export default class FileUploadPage extends React.Component {
                 <h2>File Upload</h2>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="fileInput">Choose file to upload:</label>
-                    <input type="file" id="fileInput" name="fileInput" multiple />
+                    <input type="file" id="fileInput" name="fileInput" multiple
+                        onChange={this.handleFileSelect}
+                    />
                     <button type="submit">Upload</button>
                 </form>
             </div>

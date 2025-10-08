9defaae8f1b6ed697ae04a8b77519c2fb09814c4
Zsolt Harmath <harmathzs@gmail.com>
2025-10-08 13:46:50 +0200

    Hide form if logged in

--- react-app/src/pages/LoginFormPage.jsx
+++ react-app/src/pages/LoginFormPage.jsx
@@ -38,28 +38,35 @@ export default class LoginFormPage extends React.Component {
 
   render() {
     return (
-      <div id="content-login" className="tab-content active">
-        <h2>Login</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="loginEmail">Email:</label>
-          <input type="email" id="loginEmail" name="loginEmail" autoComplete="username" 
-            onChange={this.handleEmailChange}
-          required />
-          <label htmlFor="loginPassword">Password:</label>
-          <input type="password" id="loginPassword" name="loginPassword" autoComplete="current-password" 
-            onChange={this.handlePasswordChange}
-          required />
-          <button type="submit">Login</button>
-        </form>
-        <hr style={{ margin: "24px 0" }} />
-        <div className="social-login">
-          <button type="button" className="social-btn google-login">Login with Google</button>
-          <button type="button" className="social-btn salesforce-login">Login with Salesforce</button>
-          <button type="button" className="social-btn facebook-login">Login with Facebook</button>
-        </div>
-        <hr style={{ margin: "24px 0" }} />
-        {this.state.loggedIn && <p>{this.state.email} is logged in! :) </p>}
-      </div>
+      <>
+        {this.state.loggedIn
+          ? 
+          <div id="content-login" className="tab-content active">
+            <p>{this.state.email} is logged in! </p>
+          </div>
+          : 
+          <div id="content-login" className="tab-content active">
+            <h2>Login</h2>
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="loginEmail">Email:</label>
+              <input type="email" id="loginEmail" name="loginEmail" autoComplete="username" 
+                onChange={this.handleEmailChange}
+              required />
+              <label htmlFor="loginPassword">Password:</label>
+              <input type="password" id="loginPassword" name="loginPassword" autoComplete="current-password" 
+                onChange={this.handlePasswordChange}
+              required />
+              <button type="submit">Login</button>
+            </form>
+            <hr style={{ margin: "24px 0" }} />
+            <div className="social-login">
+              <button type="button" className="social-btn google-login">Login with Google</button>
+              <button type="button" className="social-btn salesforce-login">Login with Salesforce</button>
+              <button type="button" className="social-btn facebook-login">Login with Facebook</button>
+            </div>        
+          </div>
+        }      
+      </>
     );
   }
 
